931da9241c90c6ec7d113cd6940a27b781f20c70
John Vitales <JohnieeJuiceboxx@gmail.com>
2019-11-07 22:06:45 -0500

    changed manifest

--- client/routes.js
+++ client/routes.js
@@ -29,7 +29,7 @@ class Routes extends Component {
     return (
       <Switch>
         {/* Routes placed here are available to all visitors */}
-        <Route exact path="/" component={Splash} />
+        <Route exact path="/home" component={Splash} />
         <Route path="/login" component={Login} />
         <Route path="/signup" component={Signup} />
         <Route path="/start" component={DarkLogin} />
